3e8cd6c541c2cca78ed28b323111b5bf4d24425b
Rebel <johnhartigan@ya.ru>
2020-11-22 05:30:46 +0200

    update logs

--- clients/http.ts
+++ clients/http.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 class HttpClient {
   public async get<T>(link: string): Promise<T> {
@@ -12,7 +12,7 @@ class HttpClient {
           console.log('http_client_econnrefused', error.message);
           break;
         }
-        case error?.response?.status === 502: {
+        case (error as AxiosError).response?.status === 502: {
           console.log('http_client_status_502', error.message);
           break;
         }
